a2499682a0affcb88e892cd9b17138bfa6cc4ba4
rjdbcm <rjdbcm@outlook.com>
2024-10-16 01:57:45 -0500

    perf: clean up code
    Signed-off-by: rjdbcm <rjdbcm@outlook.com>

--- tap_consumer/__init__.py
+++ tap_consumer/__init__.py
@@ -27,7 +27,7 @@ from pyparsing import Suppress
 from pyparsing import Word
 from pyparsing import empty
 from pyparsing import nums
-from pyparsing import restOfLine
+from pyparsing import rest_of_line
 
 if sys.version_info >= (3, 11):  # pragma: no cover
     from typing import Self  # noqa: TC002
@@ -38,34 +38,34 @@ __all__ = ['tap_parser', 'TAPTest', 'TAPSummary']
 
 # newlines are significant whitespace, so set default skippable
 # whitespace to just spaces and tabs
-ParserElement.setDefaultWhitespaceChars(' \t')
+ParserElement.set_default_whitespace_chars(' \t')
 NL = LineEnd().suppress()  # type: ignore
 
 integer = Word(nums)
 plan = '1..' + integer('ubound')
 
 OK, NOT_OK = map(Literal, ['ok', 'not ok'])
-testStatus = OK | NOT_OK
+test_status = OK | NOT_OK
 
 description = Regex('[^#\n]+')
-description.setParseAction(lambda t: t[0].lstrip('- '))  # pyright: ignore
+description.set_parse_action(lambda t: t[0].lstrip('- '))  # pyright: ignore
 
 TODO, SKIP = map(CaselessLiteral, 'TODO SKIP'.split())  # noqa: T101
 directive = Group(
     Suppress('#')
     + (
-        TODO + restOfLine  # noqa: T101
-        | FollowedBy(SKIP) + restOfLine.copy().setParseAction(lambda t: ['SKIP', t[0]])
+        TODO + rest_of_line  # noqa: T101
+        | FollowedBy(SKIP) + rest_of_line.copy().set_parse_action(lambda t: ['SKIP', t[0]])
     ),
 )
 
-commentLine = Suppress('#') + empty + restOfLine
+commentLine = Suppress('#') + empty + rest_of_line
 version = Suppress('TAP version') + Word(nums[1:], nums, as_keyword=True)
 yaml_end = Suppress('...')
-testLine = Group(
+test_line = Group(
     Optional(OneOrMore(commentLine + NL))('comments')
-    + testStatus('passed')
-    + Optional(integer)('testNumber')
+    + test_status('passed')
+    + Optional(integer)('test_number')
     + Optional(description)('description')
     + Optional(directive)('directive')
     + Optional(
@@ -79,18 +79,18 @@ testLine = Group(
         ),
     ),
 )
-bailLine = Group(Literal('Bail out!')('BAIL') + empty + Optional(restOfLine)('reason'))
+bail_line = Group(Literal('Bail out!')('BAIL') + empty + Optional(rest_of_line)('reason'))
 
 tap_parser = Optional(Group(Suppress(SkipTo(version)) + version)('version') + NL) + Optional(
     Group(plan)('plan') + NL,
-) & Group(OneOrMore((testLine | Suppress(SkipTo(testLine)) + testLine | bailLine) + NL))(
+) & Group(OneOrMore((test_line | Suppress(SkipTo(test_line)) + test_line | bail_line) + NL))(
     'tests',
 )
 
 
 class TAPTest:
     def __init__(self: Self, results: ParseResults) -> None:
-        self.num = results.testNumber
+        self.num = results.test_number
         self.passed = results.passed == 'ok'
         self.skipped = self.todo = False
         if results.directive:
@@ -99,7 +99,7 @@ class TAPTest:
 
     @classmethod
     def bailedTest(cls: type[Self], num: int) -> 'TAPTest':
-        ret = TAPTest(empty.parseString(''))
+        ret = TAPTest(empty.parse_string(''))
         ret.num = num
         ret.skipped = True
         return ret
@@ -107,11 +107,11 @@ class TAPTest:
 
 class TAPSummary:
     def __init__(self: Self, results: ParseResults) -> None:  # noqa: C901
-        self.passedTests = []
-        self.failedTests = []
-        self.skippedTests = []
-        self.todoTests = []
-        self.bonusTests = []
+        self.passed_tests = []
+        self.failed_tests = []
+        self.skipped_tests = []
+        self.todo_tests = []
+        self.bonus_tests = []
         self.bail = False
         self.version = results.version[0] if results.version else 12
         if results.plan:
@@ -125,33 +125,31 @@ class TAPSummary:
                 # ~ print "Test suite aborted: " + res.reason
                 # ~ self.failedTests += expected[i:]
                 self.bail = True
-                self.skippedTests += [TAPTest.bailedTest(ii) for ii in expected[i:]]
+                self.skipped_tests += [TAPTest.bailedTest(ii) for ii in expected[i:]]
                 self.bailReason = res.reason  # pyright: ignore
                 break
 
             testnum = i + 1
-            if res.testNumber != '':  # pyright: ignore
+            if res.testNumber != '':  # pragma: no cover  # pyright: ignore
                 if testnum != int(res.testNumber):  # pyright: ignore
-                    print(
-                        'ERROR! test %(testNumber)s out of sequence' % res
-                    )  # pragma: no cover
+                    print('ERROR! test %(testNumber)s out of sequence' % res)
                 testnum = int(res.testNumber)  # pyright: ignore
             res['testNumber'] = testnum  # pyright: ignore
 
             test = TAPTest(res)  # pyright: ignore
             if test.passed:
-                self.passedTests.append(test)
+                self.passed_tests.append(test)
             else:
-                self.failedTests.append(test)
+                self.failed_tests.append(test)
             if test.skipped:
-                self.skippedTests.append(test)
+                self.skipped_tests.append(test)
             if test.todo:
-                self.todoTests.append(test)
+                self.todo_tests.append(test)
             if test.todo and test.passed:
-                self.bonusTests.append(test)
+                self.bonus_tests.append(test)
 
         self.passedSuite = not self.bail and (
-            set(self.failedTests) - set(self.todoTests) == set()
+            set(self.failed_tests) - set(self.todo_tests) == set()
         )
 
     def summary(  # noqa: C901
@@ -160,25 +158,25 @@ class TAPSummary:
         testListStr = lambda tl: '[' + ','.join(str(t.num) for t in tl) + ']'  # noqa: E731
         summaryText = [f'TAP version: {self.version}']
         if showPassed or showAll:
-            summaryText.append(f'PASSED: {testListStr(self.passedTests)}')  # type: ignore
+            summaryText.append(f'PASSED: {testListStr(self.passed_tests)}')  # type: ignore
         else:  # pragma: no cover
             pass
-        if self.failedTests or showAll:
-            summaryText.append(f'FAILED: {testListStr(self.failedTests)}')  # type: ignore
+        if self.failed_tests or showAll:
+            summaryText.append(f'FAILED: {testListStr(self.failed_tests)}')  # type: ignore
         else:  # pragma: no cover
             pass
-        if self.skippedTests or showAll:
-            summaryText.append(f'SKIPPED: {testListStr(self.skippedTests)}')  # type: ignore
+        if self.skipped_tests or showAll:
+            summaryText.append(f'SKIPPED: {testListStr(self.skipped_tests)}')  # type: ignore
         else:  # pragma: no cover
             pass
-        if self.todoTests or showAll:
+        if self.todo_tests or showAll:
             summaryText.append(
-                f'TODO: {testListStr(self.todoTests)}'  # type: ignore  # noqa: T101
+                f'TODO: {testListStr(self.todo_tests)}'  # type: ignore  # noqa: T101
             )
         else:  # pragma: no cover
             pass
-        if self.bonusTests or showAll:
-            summaryText.append(f'BONUS: {testListStr(self.bonusTests)}')  # type: ignore
+        if self.bonus_tests or showAll:
+            summaryText.append(f'BONUS: {testListStr(self.bonus_tests)}')  # type: ignore
         else:  # pragma: no cover
             pass
         if self.passedSuite:
@@ -190,4 +188,4 @@ class TAPSummary:
 
 # create TAPSummary objects from tapOutput parsed results, by setting
 # class as parse action
-tap_parser.setParseAction(TAPSummary)
+tap_parser.set_parse_action(TAPSummary)
